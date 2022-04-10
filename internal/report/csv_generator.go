22cda9efdce7123d23db1fa7bf130de2e1fd08d5
David Nguyen <hpcsc@users.noreply.github.com>
2022-04-10 21:42:24 +1000

    Add remaining tests for single report generation

--- internal/report/csv_generator.go
+++ internal/report/csv_generator.go
@@ -1,6 +1,12 @@
 package report
 
-import "github.com/hpcsc/outside-in-go/internal/storer"
+import (
+	"bytes"
+	"encoding/csv"
+	"fmt"
+	"github.com/hpcsc/outside-in-go/internal/storer"
+	"log"
+)
 
 var _ Generator = &csvGenerator{}
 
@@ -15,12 +21,49 @@ type csvGenerator struct {
 }
 
 func (g *csvGenerator) GenerateSingle(year int, month int) ([]byte, error) {
-	existingAggregated, _ := g.storer.RetrieveAggregated(storer.SingleReportType, year, month)
-	if existingAggregated != nil {
+	existingAggregated, err := g.storer.RetrieveAggregated(storer.SingleReportType, year, month)
+	if err != nil {
+		log.Printf("failed to retrieved existing aggregate: %v", err)
+		// continue with aggregate logic
+	} else if existingAggregated != nil {
 		return existingAggregated, nil
 	}
 
-	return nil, nil
+	files, err := g.storer.RetrieveIndividualFiles(storer.SingleReportType, year, month)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(files) == 0 {
+		return nil, fmt.Errorf("no data available for %02d/%d", month, year)
+	}
+
+	var aggregatedLines [][]string
+	for i, f := range files {
+		reader := csv.NewReader(bytes.NewReader(f))
+		lines, err := reader.ReadAll()
+		if err != nil {
+			return nil, fmt.Errorf("failed to read one or more csv files: %v", err)
+		}
+
+		if i == 0 {
+			aggregatedLines = append(aggregatedLines, lines[0])
+		}
+
+		aggregatedLines = append(aggregatedLines, lines[1:]...)
+	}
+
+	var aggregated bytes.Buffer
+	writer := csv.NewWriter(&aggregated)
+	if err = writer.WriteAll(aggregatedLines); err != nil {
+		return nil, fmt.Errorf("failed to write csv content to buffer: %v", err)
+	}
+
+	if err := g.storer.StoreAggregated(storer.SingleReportType, year, month, aggregated.Bytes()); err != nil {
+		return nil, err
+	}
+
+	return aggregated.Bytes(), nil
 }
 
 func (g *csvGenerator) GenerateCumulative(year int, month int) ([]byte, error) {
--- internal/report/csv_generator_test.go
+++ internal/report/csv_generator_test.go
@@ -3,16 +3,19 @@
 package report
 
 import (
+	"errors"
 	"github.com/hpcsc/outside-in-go/internal/storer"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestCsvGenerator(t *testing.T) {
+	year := 2022
+	month := 4
+
 	t.Run("generate single", func(t *testing.T) {
 		t.Run("return aggregated file straightaway when found", func(t *testing.T) {
-			year := 2022
-			month := 4
 			stubStorer := storer.NewMock()
 			stubStorer.StubRetrieveAggregated(storer.SingleReportType, year, month).Return([]byte("some,data"), nil)
 			g := NewCsvGenerator(stubStorer)
@@ -23,6 +26,78 @@ func TestCsvGenerator(t *testing.T) {
 			require.Equal(t, []byte("some,data"), data)
 			stubStorer.AssertRetrieveIndividualFilesNotCalled(t)
 			stubStorer.AssertStoreAggregatedNotCalled(t)
+		})
+
+		t.Run("return error if no individual files available for given year and month", func(t *testing.T) {
+			stubStorer := storer.NewMock()
+			stubStorer.StubRetrieveAggregated(storer.SingleReportType, year, month).Return(nil, nil)
+			stubStorer.StubRetrieveIndividualFiles(storer.SingleReportType, year, month).Return([][]byte{}, nil)
+			g := NewCsvGenerator(stubStorer)
+
+			_, err := g.GenerateSingle(year, month)
+
+			require.Error(t, err)
+			require.Contains(t, err.Error(), "no data available for 04/2022")
+		})
+
+		t.Run("return error if failed to retrieve individual files", func(t *testing.T) {
+			stubStorer := storer.NewMock()
+			stubStorer.StubRetrieveAggregated(storer.SingleReportType, year, month).Return(nil, nil)
+			stubStorer.StubRetrieveIndividualFiles(storer.SingleReportType, year, month).Return(nil, errors.New("some error"))
+			g := NewCsvGenerator(stubStorer)
+
+			_, err := g.GenerateSingle(year, month)
+
+			require.Error(t, err)
+			require.Contains(t, err.Error(), "some error")
+		})
+
+		t.Run("store and return aggregated report", func(t *testing.T) {
+			stubStorer := storer.NewMock()
+			stubStorer.StubRetrieveAggregated(storer.SingleReportType, year, month).Return(nil, nil)
+			stubStorer.StubRetrieveIndividualFiles(storer.SingleReportType, year, month).Return(
+				[][]byte{
+					[]byte(`CLUSTER,DATA
+cluster-1,data-1.1
+cluster-1,data-1.2`),
+					[]byte(`CLUSTER,DATA
+cluster-2,data-2.1
+cluster-2,data-2.2`),
+				},
+				nil)
+			stubStorer.StubStoreAggregated(storer.SingleReportType, year, month, mock.Anything).Return(nil)
+			g := NewCsvGenerator(stubStorer)
+
+			data, err := g.GenerateSingle(year, month)
+
+			require.NoError(t, err)
+			stubStorer.AssertStoreAggregatedCalled(t, storer.SingleReportType, year, month, mock.Anything)
+			expecteData := []byte(`CLUSTER,DATA
+cluster-1,data-1.1
+cluster-1,data-1.2
+cluster-2,data-2.1
+cluster-2,data-2.2
+`)
+			require.Equal(t, expecteData, data)
+		})
+
+		t.Run("return error when failed to store aggregate file", func(t *testing.T) {
+			stubStorer := storer.NewMock()
+			stubStorer.StubRetrieveAggregated(storer.SingleReportType, year, month).Return(nil, nil)
+			stubStorer.StubRetrieveIndividualFiles(storer.SingleReportType, year, month).Return(
+				[][]byte{
+					[]byte(`CLUSTER,DATA
+cluster-1,data-1.1
+cluster-1,data-1.2`),
+				},
+				nil)
+			stubStorer.StubStoreAggregated(storer.SingleReportType, year, month, mock.Anything).Return(errors.New("some error"))
+			g := NewCsvGenerator(stubStorer)
+
+			_, err := g.GenerateSingle(year, month)
+
+			require.Error(t, err)
+			require.Contains(t, err.Error(), "some error")
 		})
 	})
 }
