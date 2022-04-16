c09a48340f455c999a237c2b0e53ebb67760fcaf
David Nguyen <hpcsc@users.noreply.github.com>
2022-04-16 20:31:25 +1000

    Add first storer test

---
+++ internal/storer/s3_storer.go
@@ -0,0 +1,87 @@
+package storer
+
+import (
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"io/ioutil"
+)
+
+var _ Storer = &s3Storer{}
+
+type s3Storer struct {
+	bucket string
+	client *s3.Client
+}
+
+func NewS3Storer(endpoint string, bucket string) (Storer, error) {
+	cfg, err := s3Config(endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load aws config: %v", err)
+	}
+
+	return &s3Storer{
+		bucket: bucket,
+		client: s3.NewFromConfig(cfg, func(o *s3.Options) {
+			o.UsePathStyle = true
+		}),
+	}, nil
+}
+
+func (s *s3Storer) RetrieveIndividualFiles(reportType ReportType, year int, month int) ([][]byte, error) {
+	prefix := fmt.Sprintf("%d/%02d/%s", year, month, reportType)
+	listResponse, err := s.client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String(s.bucket),
+		Prefix: aws.String(prefix),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list objects at %s: %v", prefix, err)
+	}
+
+	var result [][]byte
+	for _, f := range listResponse.Contents {
+		object, err := s.client.GetObject(context.TODO(), &s3.GetObjectInput{
+			Bucket: aws.String(s.bucket),
+			Key:    f.Key,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("failed to get object at %s: %v", *f.Key, err)
+		}
+
+		content, err := ioutil.ReadAll(object.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read object content at %s: %v", *f.Key, err)
+		}
+
+		result = append(result, content)
+	}
+
+	return result, nil
+}
+
+func (s *s3Storer) RetrieveAggregated(reportType ReportType, year int, month int) ([]byte, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *s3Storer) StoreAggregated(reportType ReportType, year int, month int, data []byte) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func s3Config(endpoint string) (aws.Config, error) {
+	return config.LoadDefaultConfig(context.TODO(),
+		config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+			if service == s3.ServiceID {
+				return aws.Endpoint{
+					PartitionID:   "aws",
+					URL:           endpoint,
+					SigningRegion: region,
+				}, nil
+			}
+			// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
+			return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+		})))
+}
---
+++ internal/storer/s3_storer_test.go
@@ -0,0 +1,101 @@
+//go:build integration
+
+package storer
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/stretchr/testify/require"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestS3Storer_RetrieveIndividualFiles(t *testing.T) {
+	t.Run("return empty and no error when no files found", func(t *testing.T) {
+		s3Endpoint := os.Getenv("S3_ENDPOINT")
+		bucket := randomName("bucket")
+
+		client := s3ClientToMockAws(t, s3Endpoint)
+		newEncryptedS3Bucket(t, client, bucket)
+
+		s, err := NewS3Storer(s3Endpoint, bucket)
+		require.NoError(t, err)
+		data, err := s.RetrieveIndividualFiles(SingleReportType, 2022, 4)
+
+		require.NoError(t, err)
+		require.Empty(t, data)
+	})
+
+	t.Run("return files when found", func(t *testing.T) {
+		s3Endpoint := os.Getenv("S3_ENDPOINT")
+		bucket := randomName("bucket")
+
+		client := s3ClientToMockAws(t, s3Endpoint)
+		newEncryptedS3Bucket(t, client, bucket)
+		putTestCsvAtPath(t, client, bucket, "2022/04/single/cluster-1.csv")
+		putTestCsvAtPath(t, client, bucket, "2022/04/single/cluster-2.csv")
+
+		s, err := NewS3Storer(s3Endpoint, bucket)
+		require.NoError(t, err)
+		data, err := s.RetrieveIndividualFiles(SingleReportType, 2022, 4)
+
+		require.NoError(t, err)
+		require.Len(t, data, 2)
+		expected := [][]byte{
+			[]byte(fmt.Sprintf("BUCKET,PATH\n%s,2022/04/single/cluster-1.csv", bucket)),
+			[]byte(fmt.Sprintf("BUCKET,PATH\n%s,2022/04/single/cluster-2.csv", bucket)),
+		}
+		require.Equal(t, expected, data)
+	})
+}
+
+func putTestCsvAtPath(t *testing.T, s3Client *s3.Client, bucket string, path string) {
+	csv := fmt.Sprintf("BUCKET,PATH\n%s,%s", bucket, path)
+	_, err := s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket:      aws.String(bucket),
+		Key:         aws.String(path),
+		ContentType: aws.String("text/csv"),
+		Body:        bytes.NewReader([]byte(csv)),
+	})
+	require.NoError(t, err)
+}
+
+func newEncryptedS3Bucket(t *testing.T, s3Client *s3.Client, bucket string) {
+	_, err := s3Client.CreateBucket(context.TODO(), &s3.CreateBucketInput{
+		Bucket: aws.String(bucket),
+		ACL:    types.BucketCannedACLPrivate,
+	})
+	require.NoError(t, err)
+
+	_, err = s3Client.PutBucketEncryption(context.TODO(), &s3.PutBucketEncryptionInput{
+		Bucket: aws.String(bucket),
+		ServerSideEncryptionConfiguration: &types.ServerSideEncryptionConfiguration{
+			Rules: []types.ServerSideEncryptionRule{
+				{
+					ApplyServerSideEncryptionByDefault: &types.ServerSideEncryptionByDefault{
+						SSEAlgorithm: types.ServerSideEncryptionAes256,
+					},
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+}
+
+func randomName(prefix string) string {
+	return fmt.Sprintf("%s-%d", prefix, time.Now().UnixNano())
+}
+
+func s3ClientToMockAws(t *testing.T, endpoint string) *s3.Client {
+	awsConfig, err := s3Config(endpoint)
+	require.NoError(t, err)
+
+	return s3.NewFromConfig(awsConfig, func(o *s3.Options) {
+		o.UsePathStyle = true
+	})
+}
